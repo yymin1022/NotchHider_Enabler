2b33166bdd7d6e1dbb7ca5ad9c11f601dcae40e4
yymin1022 <yymin1022@gmail.com>
2020-01-22 00:09:43 +0900

    Add Korean Translation

--- main.py
+++ main.py
@@ -11,39 +11,52 @@ print("####################################")
 
 strOS = platform.system()
 if strOS == "Windows":
-    print("Current OS is Windows")
+    print("PC의 운영체제는 Windows입니다.")
+    print("Current OS is Windows.\n")
     adb_path = os.path.join(sys._MEIPASS, "adb.exe")
 elif strOS == "Linux":
-    print("Current OS is Linux")
+    print("PC의 운영체제는 Linux입니다.")
+    print("Current OS is Linux.\n")
     adb_path = os.path.join(sys._MEIPASS, "adb")
 
 while(True):
-    print("Conntect Device with USB Debugging Enabled")
-    input("/* Press Any Key to Continue */")
+    print("USB 디버깅을 활성화한 뒤 USB 케이블로 기기를 연결해주세요.")
+    print("Enable USB Debugging and connect device with USB Cable.\n")
+
+    print("/* 계속하려면 아무키나 누르십시오. */")
+    input("/* Press any key to continue. */\n")
     if "product" in str(subprocess.check_output([adb_path, 'devices', '-l'])):
+        print("기기가 확인되었습니다.")
         print("Device Found.\n")
         break
     else:
+        print("기기가 확인되지 않습니다. 다시시도해주세요.")
         print("Device Not Found. Try Again.\n")
         continue
 
-print("Overscan Command will run. Please wait a moment, and DO NOT DISCONNECT DEVICE.")
-input("/* Press Any Key to Continue */")
+print("UI 조작 커맨드가 실행됩니다. 기기의 연결을 해제하지 말고 잠시만 기다려주세요.")
+print("Overscan Command will run. Please wait a moment, and DO NOT DISCONNECT DEVICE.\n")
+print("/* 계속하려면 아무키나 누르십시오. */")
+input("/* Press any key to continue. */\n")
 failCount = 0
 while(True):
     if failCount >= 3:
+        print("여러번 작업을 시도하였으나, 실패하였습니다. 기기가 지원되지 않는 것 같습니다. 개발자에게 리포트해주세요.")
         print("Failed too many time. Device might be unsupported. Please contact developer.")
-        print("Email : yymin1022@gmail.com")
+        print("Email : yymin1022@gmail.com\n")
         break
     else:
         try:
-            subprocess.check_output([adb_path, 'shell', 'wm', 'overscan', '0,0,0,0'])
+            subprocess.check_output([adb_path, 'shell', 'wm', 'overscan', '0,90,0,0'])
         except subprocess.CalledProcessError:
-            print("Failed. Trying again.")
+            print("작업에 실패하였습니다. 다시시도합니다.")
+            print("Failed. Trying again.\n")
             failCount += 1
             continue
-        print("Successfully applied. Device will be rebooted.")
+        print("성공적으로 적용되었습니다. 기기가 재부팅됩니다.")
+        print("Successfully applied. Device will be rebooted.\n")
         subprocess.check_output([adb_path, 'reboot'])
         break
 
-
+print("/* 계속하려면 아무키나 누르십시오. */")
+input("/* Press any key to continue. */\n")
